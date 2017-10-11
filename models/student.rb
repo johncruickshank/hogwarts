8e8a3f52c32a8d7fbc28aa52bd77de293111e0b2
John Cruickshank <johncruickshank87@gmail.com>
2017-10-11 13:41:14 +0100

    updates database to complete spec

--- models/student.rb
+++ models/student.rb
@@ -1,24 +1,24 @@
 require_relative("../db/sql_runner.rb")
 
 class Student
-  attr_accessor :first_name, :last_name, :house, :age
+  attr_accessor :first_name, :last_name, :house_id, :age
   attr_reader :id
 
   def initialize(options)
     @id = options['id'].to_i()
     @first_name = options['first_name']
     @last_name = options['last_name']
-    @house = options['house']
+    @house_id = options['house_id'].to_i()
     @age = options['age'].to_i()
   end
 
   def save()
     sql = "INSERT INTO students
-    (first_name, last_name, house, age)
+    (first_name, last_name, house_id, age)
     VALUES
     ($1, $2, $3, $4)
     RETURNING id"
-    values = [@first_name, @last_name, @house, @age]
+    values = [@first_name, @last_name, @house_id, @age]
     student = SqlRunner.run(sql, values).first()
     @id = student['id'].to_i()
   end
@@ -37,15 +37,25 @@ class Student
     return result
   end
 
-  def find(id)
+  def self.find(id)
     sql = "SELECT * FROM students WHERE id = $1"
     values = [id]
     student = SqlRunner.run(sql, values).first()
     return Student.new(student)
   end
 
+  def delete()
+    sql = "DELETE FROM students WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
 
-
+  def house()
+    sql = "SELECT * FROM houses WHERE id = $1"
+    values = @id
+    results = SqlRunner.run(sql, values)
+    house = results.map { |house| House.new(house) }
+  end
 
 
 end
